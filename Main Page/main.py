fbc7e03888f3df3a22266b8a8c9ef3dd8e2c1261
taisaalho <92999297+taisaalho@users.noreply.github.com>
2022-01-18 20:15:50 +0000

    Imagens main page

--- Main Page/main.py
+++ Main Page/main.py
@@ -2,6 +2,8 @@
 from tkinter import *
 from tkinter import messagebox
 import tkinter as tk
+from PIL import Image,ImageTk
+from tkinter import filedialog
 
 
 # Dimensões e Visual
@@ -24,6 +26,10 @@ Registo.place(x=750,y=25)
 Pesquisa = Button(mainWindow,text="Pesquisa",width=60,height=2)
 Pesquisa.place(x=200,y=25)
 
+#Funçoes e windows do Header
+
+
+
 #Funções dos buttons
 
 
@@ -32,7 +38,14 @@ Pesquisa.place(x=200,y=25)
 
 #Top Rated
 
-Top1 = Button(mainWindow,text="Top1",width=35,height=10,)
+#Imagem - TOP 1
+
+imagemAvatar = Image.open("Avatar.jpg")
+imagemAvatar = imagemAvatar.resize((50,50),Image.ANTIALIAS)
+imagemAvatar1 = ImageTk.PhotoImage(imagemAvatar)
+
+#Button TOP 1
+Top1 = Button(mainWindow,width=35,height=10,image=imagemAvatar1)
 Top1.place(x=59,y=380)
 top1_label = Label(mainWindow,text="NOME")
 top1_label.place(x=169,y=550)
@@ -52,8 +65,6 @@ top3_label.place(x=689,y=550)
 Recomendado = Button(mainWindow,text="Recomendado",width=119,height=15)
 Recomendado.place(x=30,y=100)
 
-
-#Imagens
 
 
 
