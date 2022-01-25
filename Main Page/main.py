0327aee2f4728800964fb04045e397bb345cdf45
taisaalho <92999297+taisaalho@users.noreply.github.com>
2022-01-25 15:18:45 +0000

    Main page - Taísa

--- Main Page/main.py
+++ Main Page/main.py
@@ -74,13 +74,13 @@ def Registar(username,password):
 
 #Header
 
-Login = Button(mainWindow, text="Login",width=8,height=2,function=LogIn)
+Login = Button(mainWindow, text="Login",width=8,height=2)
 Login.place(x=680,y=25)
 
 Registo = Button(mainWindow,text="Registo",width=8,height=2)
 Registo.place(x=750,y=25)
 
-Pesquisa = Button(mainWindow,text="Pesquisa",width=60,height=2,function=Pesquisa)
+Pesquisa = Button(mainWindow,text="Pesquisa",width=60,height=2)
 Pesquisa.place(x=200,y=25)
 
 
@@ -93,13 +93,13 @@ imgAvatar = imagemAvatar.resize((250,229), Image.ANTIALIAS)
 AvatarResized = ImageTk.PhotoImage(imgAvatar)
 
 top1_canvas = Canvas(mainWindow,width=250,height=225,bg="gray")
-top1_canvas.place(x=59,y=380)
+top1_canvas.place(x=66,y=390)
 top1_canvas.create_image(127,112,image = AvatarResized)
 
 #Button TOP 1
 
-top1_label = Button(mainWindow,text="Avatar the Last Airbend - Top Overall")
-top1_label.place(x=87,y=620)
+top1_label = Button(mainWindow,text="Avatar the Last Airbend - TOP OVERALL")
+top1_label.place(x=87,y=630)
 
 #Image TOP 2
 
@@ -108,13 +108,13 @@ imgRM = imagemRM.resize((250,229), Image.ANTIALIAS)
 RMResized = ImageTk.PhotoImage(imgRM)
 
 top2_canvas = Canvas(mainWindow,width=250,height=225,bg="gray")
-top2_canvas.place(x=324,y=380)
+top2_canvas.place(x=324,y=390)
 top2_canvas.create_image(127,112,image = RMResized)
 
 #Button TOP 2
 
-top2_label = Button(mainWindow,text="Rick and Morty - Top Series")
-top2_label.place(x=372,y=620)
+top2_label = Button(mainWindow,text="Rick and Morty - TOP SERIES")
+top2_label.place(x=372,y=630)
 
 #Imagem TOP 3
 
@@ -123,13 +123,13 @@ imgSA = imagemSA.resize((250,229), Image.ANTIALIAS)
 SAResized = ImageTk.PhotoImage(imgSA)
 
 top3_canvas = Canvas(mainWindow,width=250,height=225,bg="gray")
-top3_canvas.place(x=584,y=380)
+top3_canvas.place(x=584,y=390)
 top3_canvas.create_image(127,112,image = SAResized)
 
 #Button TOP 3
 
 top3_label = Button(mainWindow,text="Spirit Away - TOP FILME")
-top3_label.place(x=645,y=620)
+top3_label.place(x=645,y=630)
 
 
 #Recomendado
@@ -138,6 +138,15 @@ Recomendado = Button(mainWindow,text="Recomendado",width=119,height=15)
 Recomendado.place(x=30,y=100)
 
 
+#Imagem Utilizador
+
+imagemUT = Image.open("Main Page//747376.png")
+imgUT = imagemUT.resize((60,54), Image.ANTIALIAS)
+UTResized = ImageTk.PhotoImage(imgUT)
+
+utilizador_canvas = Canvas(mainWindow,width=60,height=54,bg="LightSteelBlue4",bd=0,highlightthickness=0)
+utilizador_canvas.place(x=827,y=18)
+utilizador_canvas.create_image(30,27,image = UTResized)
 
 
 mainWindow.mainloop() 
