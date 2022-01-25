0a500ba77dd6426f95ca39b5a2f65baeeb588a73
Maria João Gomes <92551145+Mariaj17@users.noreply.github.com>
2022-01-25 14:52:02 +0000

    Update Pesquisa.py

--- Pesquisa/Pesquisa.py
+++ Pesquisa/Pesquisa.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 
 window=Tk()   
 window.title('Pesquisa')
-
+global lbox_filmes
 global screenHeight
 global screenWidth
 global appHeight, appWidth
@@ -11,29 +11,13 @@ global x,y
 screenWidth = window.winfo_screenwidth()
 screenHeight = window.winfo_screenheight()
 
-appWidth = 900                             
+appWidth = 900
 appHeight = 700
 x = (screenWidth/2) - (appWidth/2)
 y = (screenHeight/2) - (appHeight/2)
 window.geometry("{:.0f}x{:.0f}+{:.0f}+{:.0f}" .format(appWidth, appHeight, int(x), int(y)))
 
 ficheiro="lista_f_s.txt"
-
-def pes_categoria():
-    if rd_1.get()==True:
-        pes_ação()
-    if rd_2.get()==True:
-        pes_animacao()
-    if rd_3.get()==True:
-        pes_comedia()
-    if rd_4.get()==True:
-        pes_fantasia()
-    if rd_5.get()==True:
-        pes_misterio()
-    if rd_a.get()==True:
-        pes_filme()
-    if rd_b.get()==True:
-        pes_serie()
 
 def w_pesquisados():
     conWindow = Toplevel()
@@ -42,12 +26,13 @@ def w_pesquisados():
     conWindow.focus_force()
     lbox_filmes=Listbox(conWindow,width=90, height=20, selectmode="single")
     lbox_filmes.place(x=8, y= 25)
-    lbox_filmes.bind("<<ListboxSelect>>", )
+    lbox_filmes.bind("<<ListboxSelect>>")
     f=open("pesquisados.txt", "r")
     ficheiro=f.readlines()
     f.close
     for linha in ficheiro:
         lbox_filmes.insert("end",linha)
+    
 
 def pes_nome():
     f=open("lista_f_s.txt", "r", encoding="utf-8")
@@ -64,110 +49,127 @@ def pes_nome():
     w_pesquisados()
 
 def pes_filme():
-    f=open("lista_f_s.txt", "r", encoding="utf-8")
-    ficheiro=f.readlines()
-    f.close()
-    for linha in ficheiro:
-        campos=linha.split(";")
-        #print(campos[2],campos[3])
-        if campos[0]=="Filme":
-            if campos[1] not in "pesquisados.txt":
-                f=open("pesquisados.txt","a")
-                frase=campos[1]+"\n"
-                f.write(frase)
-                f.close
-    w_pesquisados()
+    if rd_a.get()==True:
+        f=open("lista_f_s.txt", "r", encoding="utf-8")
+        ficheiro=f.readlines()
+        f.close()
+        for linha in ficheiro:
+            campos=linha.split(";")
+            #print(campos[2],campos[3])
+            if campos[0]=="Filme":
+                if campos[1] not in "pesquisados.txt":
+                    f=open("pesquisados.txt","a")
+                    frase=campos[1]+"\n"
+                    f.write(frase)
+                    f.close
+    pes_serie()
 
 def pes_serie():
-    f=open("lista_f_s.txt", "r", encoding="utf-8")
-    ficheiro=f.readlines()
-    f.close()
-    for linha in ficheiro:
-        campos=linha.split(";")
-        #print(campos[2],campos[3])
-        if campos[0]=="Série":
-            if campos[1] not in "pesquisados.txt":
-                f=open("pesquisados.txt","a")
-                frase=campos[1]+"\n"
-                f.write(frase)
-                f.close
+    if rd_b.get()==True:
+        f=open("lista_f_s.txt", "r", encoding="utf-8")
+        ficheiro=f.readlines()
+        f.close()
+        for linha in ficheiro:
+            campos=linha.split(";")
+            #print(campos[2],campos[3])
+            if campos[0]=="Série":
+                if campos[1] not in "pesquisados.txt":
+                    f=open("pesquisados.txt","a")
+                    frase=campos[1]+"\n"
+                    f.write(frase)
+                    f.close
     w_pesquisados()
 
 
 def pes_ação():
-    f=open("lista_f_s.txt", "r", encoding="utf-8")
-    ficheiro=f.readlines()
+    text1=""
+    f=open("pesquisados.txt","w")
+    f.write(text1)
     f.close()
-    for linha in ficheiro:
-        campos=linha.split(";")
-        #print(campos[2],campos[3])
-        if campos[2]=="Ação" or campos[3]=="Ação":
-            if campos[1] not in "pesquisados.txt":
-                f=open("pesquisados.txt","a")
-                frase=campos[1]+"\n"
-                f.write(frase)
-                f.close
-    w_pesquisados()
+    if rd_1.get()==True:
+        f=open("lista_f_s.txt", "r", encoding="utf-8")
+        ficheiro=f.readlines()
+        f.close()
+        for linha in ficheiro:
+            campos=linha.split(";")
+            #print(campos[2],campos[3])
+            if campos[2]=="Ação" or campos[3]=="Ação":
+                if campos[1] not in "pesquisados.txt":
+                    f=open("pesquisados.txt","a")
+                    frase=campos[1]+"\n"
+                    f.write(frase)
+                    f.close
+    pes_animacao()
 
 def pes_animacao():
-    f=open("lista_f_s.txt", "r", encoding="utf-8")
-    ficheiro=f.readlines()
-    f.close()
-    for linha in ficheiro:
-        campos=linha.split(";")
-        #print(campos[2],campos[3])
-        if campos[2]=="Animação" or campos[3]=="Animação":
-            if campos[1] not in "pesquisados.txt":
-                f=open("pesquisados.txt","a")
-                frase=campos[1]+"\n"
-                f.write(frase)
-                f.close
-    w_pesquisados()
+    if rd_2.get()==True:
+        f=open("lista_f_s.txt", "r", encoding="utf-8")
+        ficheiro=f.readlines()
+        f.close()
+        for linha in ficheiro:
+            campos=linha.split(";")
+            #print(campos[2],campos[3])
+            if campos[2]=="Animação" or campos[3]=="Animação":
+                if campos[1] not in "pesquisados.txt":
+                    f=open("pesquisados.txt","a")
+                    frase=campos[1]+"\n"
+                    f.write(frase)
+                    f.close
+    pes_comedia()
 
 def pes_comedia():
-    f=open("lista_f_s.txt", "r", encoding="utf-8")
-    ficheiro=f.readlines()
-    f.close()
-    for linha in ficheiro:
-        campos=linha.split(";")
-        #print(campos[2],campos[3])
-        if campos[2]=="Comédia" or campos[3]=="Comédia":
-            if campos[1] not in "pesquisados.txt":
-                f=open("pesquisados.txt","a")
-                frase=campos[1]+"\n"
-                f.write(frase)
-                f.close
-    w_pesquisados()
+    if rd_3.get()==True:
+        f=open("lista_f_s.txt", "r", encoding="utf-8")
+        ficheiro=f.readlines()
+        f.close()
+        for linha in ficheiro:
+            campos=linha.split(";")
+            #print(campos[2],campos[3])
+            if campos[2]=="Comédia" or campos[3]=="Comédia":
+                if campos[1] not in "pesquisados.txt":
+                    f=open("pesquisados.txt","a")
+                    frase=campos[1]+"\n"
+                    f.write(frase)
+                    f.close
+    pes_fantasia()
 
 def pes_fantasia():
-    f=open("lista_f_s.txt", "r", encoding="utf-8")
-    ficheiro=f.readlines()
-    f.close()
-    for linha in ficheiro:
-        campos=linha.split(";")
-        #print(campos[2],campos[3])
-        if campos[2]=="Fantasia" or campos[3]=="Fantasia":
-            if campos[1] not in "pesquisados.txt":
-                f=open("pesquisados.txt","a")
-                frase=campos[1]+"\n"
-                f.write(frase)
-                f.close
-    w_pesquisados()
+    if rd_4.get()==True:
+        f=open("lista_f_s.txt", "r", encoding="utf-8")
+        ficheiro=f.readlines()
+        f.close()
+        for linha in ficheiro:
+            campos=linha.split(";")
+            #print(campos[2],campos[3])
+            if campos[2]=="Fantasia" or campos[3]=="Fantasia":
+                if campos[1] not in "pesquisados.txt":
+                    f=open("pesquisados.txt","a")
+                    frase=campos[1]+"\n"
+                    f.write(frase)
+                    f.close
+    pes_misterio()
     
 def pes_misterio():
-    f=open("lista_f_s.txt", "r", encoding="utf-8")
-    ficheiro=f.readlines()
-    f.close()
-    for linha in ficheiro:
-        campos=linha.split(";")
-        #print(campos[2],campos[3])
-        if campos[2]=="Mistério" or campos[3]=="Mistério":
-            if campos[1] not in "pesquisados.txt":
-                f=open("pesquisados.txt","a")
-                frase=campos[1]+"\n"
-                f.write(frase)
-                f.close
-    w_pesquisados()
+    if rd_5.get()==True:
+        f=open("lista_f_s.txt", "r", encoding="utf-8")
+        ficheiro=f.readlines()
+        f.close()
+        for linha in ficheiro:
+            campos=linha.split(";")
+            #print(campos[2],campos[3])
+            if campos[2]=="Mistério" or campos[3]=="Mistério":
+                if campos[1] not in "pesquisados.txt":
+                    f=open("pesquisados.txt","a")
+                    frase=campos[1]+"\n"
+                    f.write(frase)
+                    f.close
+    pes_filme()
+
+def select_filme():
+    indice = lbox_filmes.curselection()   # Índice da linha selecionada
+    texto = lbox_filmes.get(indice)       # Obter conteudo da Listbox, linha selecionada 
+    filme.set(texto)
+    print(lbox_filmes.curselection())
 
 #Menu Window principal
 barra_Menu=Menu(window)
@@ -178,6 +180,7 @@ barra_Menu.add_command(label="Sair", command=window.quit)
 
 window.configure(menu=barra_Menu)
 
+filme = StringVar()
 rd_a=IntVar()
 rd_b=IntVar()
 rd_1=IntVar()
@@ -209,7 +212,7 @@ frame_categoria=LabelFrame(window, text="Categoria", width=150, height=130, reli
 frame_categoria.place(x=10, y=170)
 
 #Botão das categorias
-button_categorias=Button(window, text="Pesquisar por Categorias", width=20, height=2, fg="black", relief="raised", command=pes_categoria)
+button_categorias=Button(window, text="Pesquisa", width=20, height=2, fg="black", relief="raised", command=pes_ação)
 button_categorias.place(x=230, y=210)
 
 #Seleção das categorias
