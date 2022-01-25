6e516c976dd0e397365ca7e3c042c37f2ed78d33
Maria João Gomes <92551145+Mariaj17@users.noreply.github.com>
2022-01-25 22:57:16 +0000

    Update comentarios.py

--- comentarios.py
+++ comentarios.py
@@ -50,30 +50,30 @@ window.title("Comentários")
 coment = StringVar()
 
 #Button
-btn_guardar=Button(window, text="Comentar", width=20, height=2 , fg="blue", command=comment)
-btn_guardar.place(x=20, y=30)
+btn_guardar=Button(com_space, text="Comentar", width=10, height=2 , fg="blue", command=comment)
+btn_guardar.place(x=15, y=30)
 
-btn_limpar=Button(window, text="Apagar", width=20, height=2, fg="blue", command=remover)
-btn_limpar.place(x=220, y=30)
+btn_limpar=Button(com_space, text="Apagar", width=10, height=2, fg="blue", command=remover)
+btn_limpar.place(x=120, y=30)
 
-btn_ler=Button(window, text="Ver comentarios", width=20, height=2, fg="blue", command=read_comment)
-btn_ler.place(x=420, y=30)
+btn_ler=Button(com_space, text="Ver comentarios", width=15, height=2, fg="blue", command=read_comment)
+btn_ler.place(x=220, y=30)
 
 #Label
-lbl_dcom=Label(window, text="Deixe aqui o seu comentario:", bd=2)
-lbl_dcom.place(x=20,y=80)
+lbl_dcom=Label(com_space, text="Deixe aqui o seu comentario:", bd=2)
+lbl_dcom.place(x=15,y=80)
 
 #text
-txt_coment=Text(window, width=40, height=3, relief="sunken", bd=3)
-txt_coment.place(x=20, y=100)
+txt_coment=Text(com_space, width=40, height=3, relief="sunken", bd=3)
+txt_coment.place(x=15, y=100)
 
 #Label
-lbl_com=Label(window, text="Comentários", bd=2)
-lbl_com.place(x=20,y=157)
+lbl_com=Label(com_space, text="Comentários", bd=2)
+lbl_com.place(x=15,y=157)
 
 #listbox
-lbox_coment=Listbox(window, width=60, height=10, bd="3", selectmode = "single", selectbackground="blue")
-lbox_coment.place(x=20, y= 180)
+lbox_coment=Listbox(com_space, width=55, height=12, bd="3", selectmode = "single", selectbackground="blue")
+lbox_coment.place(x=15, y= 180)
 lbox_coment.bind("<<ListboxSelect>>", selecao_item)
 
 window.mainloop()
