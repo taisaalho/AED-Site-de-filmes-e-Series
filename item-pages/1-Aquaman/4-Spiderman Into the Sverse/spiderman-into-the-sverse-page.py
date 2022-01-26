d075eab2f2c459b0daea175f6db123f34d3fdc7e
Maria João Gomes <92551145+Mariaj17@users.noreply.github.com>
2022-01-26 00:48:10 +0000

    Update spiderman-into-the-sverse-page.py

--- item-pages/1-Aquaman/4-Spiderman Into the Sverse/spiderman-into-the-sverse-page.py
+++ item-pages/1-Aquaman/4-Spiderman Into the Sverse/spiderman-into-the-sverse-page.py
@@ -100,8 +100,62 @@ def salvar_alteracoes():
         f.write(frase)
         f.close
         
-    
+def comment():
+    linha=txt_coment.get("1.0","end")
+    ficheiro="com_spiderman.txt"
+    f=open(ficheiro,"w", encoding="utf-8")
+    f.write(linha)
+    f.close
+    f=open(ficheiro,"r",encoding="utf-8")
+    ficheiro=f.readlines()
+    f.close
+    for linha in ficheiro:
+        lbox_coment.insert("end",linha)
+    txt_coment.delete("1.0","end")
+    txt_coment("end", "")
 
+def selecao_item(event):
+    indice = lbox_coment.curselection()
+    texto = lbox_coment.get(indice)
+    coment.set(texto)
+
+def remover():
+    lbox_coment.delete(lbox_coment.curselection())
+    coment.set("")
+    f = open("com_spiderman.txt", "w", encoding="utf-8")
+    cont = lbox_coment.size()
+    for i in range(cont):
+        com = lbox_coment.get(i) 
+        if com.find("\n") == -1:
+            com = com + "\n"
+        f.write(com)
+    f.close()
+
+def read_comment():
+    f=open("com_spiderman.txt", "r")
+    ficheiro=f.readlines()
+    f.close()
+    for linha in ficheiro:
+        lbox_coment.insert("end",linha)
+    txt_coment.delete("1.0","end")
+    txt_coment("end", "")
+
+def num_likes():
+    f=open("like_spiderman.txt", "r", encoding="utf-8")
+    ficheiro=f.readlines()
+    f.close()
+    count=0
+    for linha in ficheiro:
+        count=count+1
+    count_likes.set(str(count))
+
+def give_like():
+    if rd_like.get()==True:
+        f=open("like_spiderman.txt","a")
+        frase="Like"+"\n"
+        f.write(frase)
+        f.close
+    num_likes()    
 
 #Button salva dados do filme
 favButton = Button(window, text="Salvar alterações",bg = "#FA6B6B",fg = "black",relief = "solid",bd = 2,font = "Arial 11",command=salvar_alteracoes)
@@ -115,8 +169,49 @@ eval_space.place(x=720, y=20)
 like_space = LabelFrame(eval_space, text="Likes", width=380, height=60, bd=2, relief="raised", fg="red", font=("Calisto MT", "10", "bold"))
 like_space.place(x=15, y=15)
 
+rd_like=IntVar()
+
+rd_like=Checkbutton(like_space,text="Like",font=("Helvetica", 10) ,variable=rd_like, command=give_like)
+rd_like.place(x=15,y=7)
+
+lbl_count_likes=Label(like_space, text="Nº de Likes:", font=("Helvetica", 9))
+lbl_count_likes.place(x=70, y=10)
+
+count_likes = StringVar()
+txt_count_likes=Entry(like_space, width = 5, textvariable= count_likes)
+txt_count_likes.place(x=140, y=10)
+
 #Campo de comentários
 com_space = LabelFrame(eval_space, text="Comentários", width=380, height=410, bd=2, relief="raised", fg="red", font=("Calisto MT", "10", "bold"))
 com_space.place(x=15, y=85)
+
+coment = StringVar()
+
+#Button
+btn_guardar=Button(com_space, text="Comentar", width=10, height=2 , fg="blue", command=comment)
+btn_guardar.place(x=15, y=30)
+
+btn_limpar=Button(com_space, text="Apagar", width=10, height=2, fg="blue", command=remover)
+btn_limpar.place(x=120, y=30)
+
+btn_ler=Button(com_space, text="Ver comentarios", width=15, height=2, fg="blue", command=read_comment)
+btn_ler.place(x=220, y=30)
+
+#Label
+lbl_dcom=Label(com_space, text="Deixe aqui o seu comentario:", bd=2)
+lbl_dcom.place(x=15,y=80)
+
+#text
+txt_coment=Text(com_space, width=40, height=3, relief="sunken", bd=3)
+txt_coment.place(x=15, y=100)
+
+#Label
+lbl_com=Label(com_space, text="Comentários", bd=2)
+lbl_com.place(x=15,y=157)
+
+#listbox
+lbox_coment=Listbox(com_space, width=55, height=12, bd="3", selectmode = "single", selectbackground="blue")
+lbox_coment.place(x=15, y= 180)
+lbox_coment.bind("<<ListboxSelect>>", selecao_item)
 
 window.mainloop()
