6e674ec4ac18a66c95e8f8218805e91dc0124a51
taisaalho <92999297+taisaalho@users.noreply.github.com>
2022-01-26 03:50:00 +0000

    Connect

--- Main Page/main.py
+++ Main Page/main.py
@@ -1,9 +1,10 @@
-
 from tkinter import *
 from tkinter import messagebox
 import tkinter as tk
 from PIL import Image,ImageTk
 from tkinter import filedialog
+from tkinter import ttk # treeview
+
 
 
 # Dimensões e Visual
@@ -12,7 +13,6 @@ mainWindow=tk.Tk()
 mainWindow.title("You Chose!")        #Nome do site
 mainWindow.resizable(0,0)
 mainWindow.geometry("900x700")
-mainWindow.iconbitmap() #Icon 
 mainWindow.configure(bg="LightSteelBlue4")
 
 
@@ -83,8 +83,13 @@ Registo.place(x=750,y=25)
 Pesquisa = Button(mainWindow,text="Pesquisa",width=60,height=2)
 Pesquisa.place(x=200,y=25)
 
+Logo = Label(mainWindow,text="You Chose!",width=11,height=2,font="Helvica",bg="LightSteelBlue4",highlightthickness=0)
+Logo.place(x=45,y=25)
 
 #Top Rated
+
+TRlabel = Label(mainWindow,text="TOP RATED",relief=SUNKEN)
+TRlabel.place(x=420,y=370)
 
 #Imagem - TOP 1
 
@@ -93,13 +98,13 @@ imgAvatar = imagemAvatar.resize((250,229), Image.ANTIALIAS)
 AvatarResized = ImageTk.PhotoImage(imgAvatar)
 
 top1_canvas = Canvas(mainWindow,width=250,height=225,bg="gray")
-top1_canvas.place(x=66,y=390)
+top1_canvas.place(x=64,y=400)
 top1_canvas.create_image(127,112,image = AvatarResized)
 
 #Button TOP 1
 
 top1_label = Button(mainWindow,text="Avatar the Last Airbend - TOP OVERALL")
-top1_label.place(x=87,y=630)
+top1_label.place(x=86,y=640)
 
 #Image TOP 2
 
@@ -108,13 +113,13 @@ imgRM = imagemRM.resize((250,229), Image.ANTIALIAS)
 RMResized = ImageTk.PhotoImage(imgRM)
 
 top2_canvas = Canvas(mainWindow,width=250,height=225,bg="gray")
-top2_canvas.place(x=324,y=390)
+top2_canvas.place(x=324,y=400)
 top2_canvas.create_image(127,112,image = RMResized)
 
 #Button TOP 2
 
 top2_label = Button(mainWindow,text="Rick and Morty - TOP SERIES")
-top2_label.place(x=372,y=630)
+top2_label.place(x=372,y=640)
 
 #Imagem TOP 3
 
@@ -123,19 +128,47 @@ imgSA = imagemSA.resize((250,229), Image.ANTIALIAS)
 SAResized = ImageTk.PhotoImage(imgSA)
 
 top3_canvas = Canvas(mainWindow,width=250,height=225,bg="gray")
-top3_canvas.place(x=584,y=390)
+top3_canvas.place(x=584,y=400)
 top3_canvas.create_image(127,112,image = SAResized)
 
 #Button TOP 3
 
 top3_label = Button(mainWindow,text="Spirit Away - TOP FILME")
-top3_label.place(x=645,y=630)
+top3_label.place(x=648,y=640)
 
 
 #Recomendado
 
-Recomendado = Button(mainWindow,text="Recomendado",width=119,height=15)
-Recomendado.place(x=30,y=100)
+Recomendo = LabelFrame(mainWindow,height=255,width=390,relief="sunken",text="RECOMENDADO A VER!!")
+Recomendo.place(x=30,y=100)
+
+imagemKO = Image.open("Main Page//knives_out_ver13.jpg")
+imgKO = imagemKO.resize((235,200), Image.ANTIALIAS)
+KOResized = ImageTk.PhotoImage(imgKO)
+
+nov_canvas = Canvas(Recomendo,width=230,height=200,bg="gray")
+nov_canvas.place(x=80,y=20)
+nov_canvas.create_image(115,100,image = KOResized)
+
+Recomendado = Button(Recomendo,width=10,height=1,text="Knives Out",command="8-Knives Out")     
+Recomendado.place(x=156,y=10)
+
+
+#Novo
+
+Novo = LabelFrame(mainWindow,height=255,width=390,relief="sunken",text="NOVO!!")
+Novo.place(x=480,y=100)
+
+imagemSB = Image.open("Main Page//shadow-bone.jpg")
+imgSB = imagemSB.resize((235,200), Image.ANTIALIAS)
+SBResized = ImageTk.PhotoImage(imgSB)
+
+nov_canvas = Canvas(Novo,width=230,height=200,bg="gray")
+nov_canvas.place(x=80,y=20)
+nov_canvas.create_image(115,100,image = SBResized)
+
+NoV = Button(Novo,width=10,height=1,text="Shadow Bone")
+NoV.place(x=156,y=10)
 
 
 #Imagem Utilizador
---
+++ Utilizador/PaginaUtilizador.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from tkinter import messagebox
+import tkinter as tk
+from PIL import Image,ImageTk
+from tkinter import filedialog
+
+
+# Dimensões e Visual
+
+mainWindow=tk.Tk()
+mainWindow.title("You Chose!")        #Nome do site
+mainWindow.resizable(0,0)
+mainWindow.geometry("900x700")
+mainWindow.configure(bg="LightSteelBlue4")
+
+
+#Funções
+
+#Lista Fav
+
+
+
+#Lista 
+
+mainWindow.mainloop()
