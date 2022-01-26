49c5ecbfb3c008e33c3f03d8806d9b3048020bc7
40210315 <92998384+40210315@users.noreply.github.com>
2022-01-26 08:46:42 +0000

    Update whiteWallFunction.py

--- function-code/whiteWallFunction.py
+++ function-code/whiteWallFunction.py
@@ -1,12 +1,12 @@
 from tkinter import *
-##from tkVideoPlayer import TkinterVideo
+from tkVideoPlayer import TkinterVideo
 
 def function3():
     page = Toplevel()
     page.geometry("1150x540")
     page.title("White Wall")
     page.resizable(0, 0)
-    ##page.iconbitmap("imagens\Martynamru-Leather-Movie.ico")
+    page.iconbitmap("imagens\Martynamru-Leather-Movie.ico")
 
 
     #Canvas para póster
@@ -14,8 +14,8 @@ def function3():
     poster_space.place(x=20, y=20)
 
     #Imagem póster
-    ##img = PhotoImage(file="white-wall-poster.gif")
-    ##poster_space.create_image(253, 375, image=img)
+    img = PhotoImage(file="white-wall-poster.gif")
+    poster_space.create_image(253, 375, image=img)
 
     #Campo de título
     titulo = Label(page, text="White Wall (2010)", fg="#5E239D", font=("Calisto MT", "16", "bold"))
@@ -53,15 +53,15 @@ def function3():
     trailer_space = Canvas(page, bg="gray", width=370, height=268, bd=2, relief="sunken")
     trailer_space.place(x=335, y=250)
 
-    ##videoplayer = TkinterVideo(trailer_space, scaled=True, pre_load=False)
-    ##videoplayer.load(r"white-wall-trailer.mp4")
+    videoplayer = TkinterVideo(trailer_space, scaled=True, pre_load=False)
+    videoplayer.load(r"white-wall-trailer.mp4")
 
     #Buttons play/pause
-    ##def playVideo() :
-    ##    videoplayer.play()
+    def playVideo() :
+        videoplayer.play()
 
-    ##def pauseVideo():
-    ##    videoplayer.pause()
+    def pauseVideo():
+        videoplayer.pause()
 
     btn_play = Button(trailer_space, bg="blue", text="Play", height=1, width=8, fg="white", font=("Calisto MT", "8", "bold"))#, command = playVideo)
     btn_play.place(x=20, y=230)
